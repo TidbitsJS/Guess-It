a60c3da4f9d0737729c929db7aed6d2430c96746
Sujata Gunale <tidbitsjs@gmail.com>
2020-11-17 22:38:15 +0530

    start screen layout

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
+import StartScreen from './screens/StartScreen';
 
 export default function App() {
   return (
     <View style={styles.screen}>
       <StatusBar style="auto" />
       <Header title="Guess a Number" />
+      <StartScreen />
     </View>
   );
 }
